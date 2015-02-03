20587d652a6dddcfd0781e5721c24b92d0652197
cjwebb <colinjameswebb@hotmail.com>
2015-02-03 20:28:49 +0000

    Change Error Handling + Post Job Form
    - Moved New Job Form to separate URL, as it looks nicer.
    - Added TinyMCE usage when inputting job descriptions.
    - Changed error handling to a more Golang idiomatic version.

--- src/github.com/cjwebb/job-thingo/main.go
+++ src/github.com/cjwebb/job-thingo/main.go
@@ -69,105 +69,108 @@ func startApp(database db.Database, baseUrl string) {
 	newId := func() string {
 		return uuid.NewV4().String()
 	}
+	putJob := func(r render.Render, job db.Job) {
+		err := database.PutJob(job)
+		if err != nil {
+			r.HTML(503, "/error", map[string]interface{}{"code": 503})
+		} else {
+			r.Redirect("/a/"+job.Id+"?display=newLink")
+		}
+	}
 
-	// Home page
 	m.Get("/", func(r render.Render) {
 		data := map[string]interface{}{"job": JobForm{}}
 		r.HTML(200, "home", data)
 	})
 
-	// Generate new Job and redirect
-	m.Post("/gen-job", binding.Form(JobForm{}), func(form JobForm, r render.Render, errs binding.Errors) {
+	m.Get("/jobs/post", func(r render.Render){
+		data := map[string]interface{}{"job": JobForm{}}
+		r.HTML(200, "post-job", data)
+	})
+
+	m.Post("/jobs/post", binding.Form(JobForm{}), func(form JobForm, r render.Render, errs binding.Errors) {
 		saneForm := sanitizeForm(form)
 		if errs != nil {
 			response := map[string]interface{}{"HasErrors": true, "Errors": errs, "job": saneForm}
-			r.HTML(400, "home", response)
-		} else {
-			id := newId()
-			genJob := db.Job{
-				Id: id,
-				Title: saneForm.Title,
-				Description: saneForm.Description,
-				ContactEmail: saneForm.ContactEmail,
-				Rate: saneForm.Rate,
-				JobType: saneForm.JobType,
-				JobConsList: []db.JobRef{db.JobRef{Id: id, Email: saneForm.UserEmail}},
-			}
-			err := database.PutJob(genJob)
-			if err != nil {
-				r.HTML(503, "error", map[string]interface{}{"code": 503})
-			} else {
-				r.Redirect("/a/" + id)
-			}
+			r.HTML(400, "post-job", response)
+			return
 		}
+
+		id := newId()
+		genJob := db.Job{
+			Id: id,
+			Title: saneForm.Title,
+			Description: saneForm.Description,
+			ContactEmail: saneForm.ContactEmail,
+			Rate: saneForm.Rate,
+			JobType: saneForm.JobType,
+			JobConsList: []db.JobRef{db.JobRef{Id: id, Email: saneForm.UserEmail}},
+		}
+		putJob(r, genJob)
 	})
 
-	// Show Job
 	m.Get("/a/:jobid", func(req *http.Request, params martini.Params, r render.Render) {
 		jobId, err := uuid.FromString(params["jobid"])
 		if err != nil {
 			// input not a UUID, so don't try database lookup
 			r.Redirect("/")
+			return
+		}
+
+		job, err := database.GetJob(jobId.String())
+		if err != nil {
+			log.Print(err.Error())
+			r.Redirect("/")
+			return
+		}
+
+		// if this is a brand new link, then display a panel including the URL
+		if req.URL.Query().Get("display") == "newLink" {
+			response := map[string]interface{}{"job":job, "displayLink": baseUrl + req.URL.Path}
+			r.HTML(200, "job", response)
 		} else {
-			job, err := database.GetJob(jobId.String())
-			if err != nil {
-				log.Print(err.Error())
-				r.Redirect("/")
-			} else {
-				// if this is a brand new link, then display a panel including the URL
-				if req.URL.Query().Get("display") == "newlink" {
-					response := map[string]interface{}{"job":job, "displayLink": baseUrl + req.URL.Path}
-					r.HTML(200, "job", response)
-				} else {
-					response := map[string]interface{}{"job":job}
-					r.HTML(200, "job", response)
-				}
-			}
+			response := map[string]interface{}{"job":job}
+			r.HTML(200, "job", response)
 		}
 	})
 
-	// Generate new link to Job
 	m.Post("/a/:jobid/gen-link", binding.Form(EmailForm{}), func(form EmailForm, params martini.Params, r render.Render, errs binding.Errors) {
-		// todo(cjwebb) refactor all the error handling. is duplicated in other handlers too.
 		jobId, err := uuid.FromString(params["jobid"])
 		if err != nil {
 			// input not a UUID, so don't try database lookup
 			r.Redirect("/")
-		} else {
-			job, err := database.GetJob(jobId.String())
-			if err != nil {
-				log.Print(err.Error())
-				r.Redirect("/")
-			} else {
-				saneForm := sanitizeEmailForm(form)
-				if errs != nil {
-					response := map[string]interface{}{"HasErrors": true, "Errors": errs, "link": saneForm, "job": job}
-					r.HTML(400, "job", response)
-				} else {
-					// job exists, form is valid... so gen new link
-					id := newId()
-					newRef := db.JobRef{Id: id, Email: saneForm.Email}
-					genJob := db.Job{
-						Id: id,
-						Title: job.Title,
-						Description: job.Description,
-						ContactEmail: job.ContactEmail,
-						Rate: job.Rate,
-						JobType: job.JobType,
-						JobConsList: append([]db.JobRef{newRef}, job.JobConsList...),
-					}
-					err := database.PutJob(genJob)
-					if err != nil {
-						r.HTML(503, "error", map[string]interface{}{"code": 503})
-					} else {
-						r.Redirect("/a/" + id + "?display=newlink")
-					}
-				}
-			}
+			return
 		}
+
+		job, err := database.GetJob(jobId.String())
+		if err != nil {
+			log.Print(err.Error())
+			r.Redirect("/")
+			return
+		}
+
+		saneForm := sanitizeEmailForm(form)
+		if errs != nil {
+			response := map[string]interface{}{"HasErrors": true, "Errors": errs, "link": saneForm, "job": job}
+			r.HTML(400, "job", response)
+			return
+		}
+
+		// job exists, form is valid... so gen new link
+		id := newId()
+		newRef := db.JobRef{Id: id, Email: saneForm.Email}
+		genJob := db.Job{
+			Id: id,
+			Title: job.Title,
+			Description: job.Description,
+			ContactEmail: job.ContactEmail,
+			Rate: job.Rate,
+			JobType: job.JobType,
+			JobConsList: append([]db.JobRef{newRef}, job.JobConsList...),
+		}
+		putJob(r, genJob)
 	})
 
-	// About page
 	m.Get("/about", func(r render.Render) {
 		r.HTML(200, "about", nil)
 	})
